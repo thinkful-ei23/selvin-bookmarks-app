2977f5e95c394f6d181844d98c3df20921593215
Selvin Norman <selvin715@gmail.com>
2018-08-29 15:11:14 -0700

    updated for resub

--- scripts/api.js
+++ scripts/api.js
@@ -5,18 +5,27 @@ const api = (function() {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/selvin/bookmarks';
 
   const getBookmarks = function(callback) {
-    console.log ('getBookmarks ran');
-    $.getJSON(`${BASE_URL}`, callback);
+    //console.log ('getBookmarks ran');
+    $.getJSON({
+      url: `${BASE_URL}`, 
+      success: callback,
+      error: function(xhr){
+      console.log("Error! " + xhr.responseText);
+      //if (xhr.responseText === '{"message":"Attributes `title` and `url` required"}') {
+      store.errmsg = xhr.responseText.slice(22, 59 );  
+      //}
+      }
+    });
   };
 
   const createBookmark = function(title, url, desc, rating, callback) {
-    const newBookmark = {
+    const newBm = {
         title: title,
         url: url,
         desc: desc,
         rating: rating
     };
-    const strJSON = JSON.stringify(newBookmark);
+    const strJSON = JSON.stringify(newBm);
     console.log(`strJSON: ${strJSON}`);
     // With ajax()
     $.ajax({
@@ -25,18 +34,63 @@ const api = (function() {
       dataType: 'json',
       contentType: 'application/json',
       data: strJSON,
-      success: callback
+      success:  callback,
+      error: function(xhr){
+        if  (xhr.status === 0){
+          console.log('store.errmsg: ' + store.errmsg);   
+          store.errmsg = 'Title must be provided!';  
+          bookmarkList.handleErrorMessage('Title must be provided!');
+          //$('#create-bm').prepend(`<p style="color:red;font-weight:bold;">'Title must be provided!'</p>`);
+          //callback;
+        
+        } else {
+          if (xhr.status === 400) {
+            store.errmsg = xhr.responseText.slice(22, 49 );  
+            //$('#create-bm').prepend(`<p style="color:red;font-weight:bold;">${store.errmsg}</p>`);
+            console.log('store.errmsg: ' + store.errmsg);    
+          } else {
+            console.log("Error! " + xhr.status + " | " + xhr.responseText);
+            store.errmsg = xhr.responseText.slice(22, 49 ); 
+            bookmarkList.handleErrorMessage(store.errmsg);           
+            //$('#create-bm').prepend(`<p class="errmsg">${store.errmsg}</p>`);
+          }
+        }      
+        console.log("Error! " + xhr.status + " | " + xhr.responseText);
+        store.errmsg = xhr.responseText.slice(22, 49 ); 
+        bookmarkList.handleErrorMessage(store.errmsg); 
+      }
     });
   };
 
   const updateBookmark = function(id, updateData, callback) {
-    $.ajax({
+  $.ajax({
       url: `${BASE_URL}/${id}`,
       method: 'PATCH',
       dataType: 'json',
       contentType: 'application/json',
       data: JSON.stringify(updateData),
-      success: callback
+      success: callback,
+      error: function(xhr){
+        console.log("Error! " + xhr.responseText);
+        //if (xhr.responseText === '{"message":"Attributes `title` and `url` required"}') {
+        //  store.errmsg = xhr.responseText.slice(22, 59 );  
+        //}
+      }
+    });
+  };
+
+  const deleteBookmark = function(id, callback) {
+    $.ajax({
+      url: `${BASE_URL}/${id}`,
+      method: 'DELETE',
+      dataType: 'json',
+      contentType: 'application/json',
+      error: function(xhr){
+        console.log("Error! " + xhr.responseText);
+        // if (xhr.responseText === '{"message":"Attributes `title` and `url` required"}') {
+         store.errmsg = xhr.responseText.slice(22, 59 );  
+        // }
+      }
     });
   };
 
@@ -44,5 +98,6 @@ const api = (function() {
     getBookmarks,
     createBookmark,  
     updateBookmark,
+    deleteBookmark,
   };
 }());
